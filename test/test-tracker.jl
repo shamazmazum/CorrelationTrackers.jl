4c6aa08692174b11a95ec76725a5294653ed1237
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-05-18 06:21:56 +0000

    Unit tests

--- test/test-tracker.jl
+++ test/test-tracker.jl
@@ -1,1 +1,30 @@
-@test 1==1
+function test_tracker!(array :: AbstractArray)
+    for p in (false, true)
+        for phase in (0, 1)
+            tracker = CorrelationTracker(array, phase; periodic = p)
+            for n in 1:20
+                idx = [rand(1:d) for d in size(array)]
+                array[idx...] = 1 - array[idx...]
+                tracker[idx...] = 1 - tracker[idx...]
+            end
+
+            for func in (Directional.l2, Directional.s2)
+                expected = func(array, phase; periodic = p)
+                got = func(tracker)
+                for (direction, data) in expected
+                    @test data ≈ got[direction]
+                end
+            end
+        end
+    end
+end
+
+@testset "2D system" begin
+    array = rand(0:1, (100, 50))
+    test_tracker!(array)
+end
+
+@testset "3D system" begin
+    array = rand(0:1, (100, 50, 200))
+    test_tracker!(array)
+end
